634f22edee62034c50a737fb48465f7ecff637e2
Judah Sotomayor <judah.sotomayor@pm.me>
2025-11-17 11:43:30 -0500

    feat: Add ulfius.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "ulfius Web Framework";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    ulfius = {
+      url = "git+https://github.com/babelouest/ulfius";
+      flake = false;
+    };
+  };
+
+  outputs = {self, nixpkgs, ulfius} :
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system} system);
+    in
+      {
+        packages = forAllSystems (pkgs: system: {
+          default = pkgs.stdenv.mkDerivation {
+            name = "ulfius";
+            src = ulfius;
+            outputs = ["out"];
+            buildInputs = with pkgs;[
+              jansson
+              curl
+              libmicrohttpd
+              gnutls
+              orcania
+              yder
+              pkg-config
+              check
+            ];
+
+            installPhase = ''
+              mkdir -p $out/lib
+              make DESTDIR=$out install;
+            '';
+            checkPhase = "make check";
+          };
+        });
+      };
+}
